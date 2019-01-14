dcf5141453060960426ed93d872b4cd3232b82c0
alokkesari <arkesari@iith.ac.in>
2019-01-14 22:07:44 +0530

    support for all linux systems

---
+++ README.md
@@ -0,0 +1,1 @@
+hfhggbmnjhn
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-
+sudo apt-get install libusb-1.0-0-dev
+cd usbboot && sudo make
+sudo cp rpiboot ../rpiboot
 sudo rm `pwd`/boot/kernel.img
 echo "-------------------------Removing previous existing versions of kernel.img.-------------------------"
 sudo rm `pwd`/{main.elf,start.o,main.o}
@@ -9,10 +11,10 @@ echo "-------------------------Installing compiler for generating the kernel ima
 arm-none-eabi-as start.S -o start.o
 echo "-------------------------Generating object file from Assembly code source file.-------------------------"
 arm-none-eabi-gcc -Wall -Werror -O2 -nostdlib -nostartfiles -ffreestanding -c main.c -o main.o
-echo "-------------------------Generating object file from the maine program written in Embedded C.-------------------------"
+echo "-------------------------Generating object file from the main program written in Embedded C.-------------------------"
 arm-none-eabi-ld start.o main.o -T ldscript -o main.elf
 echo "-------------------------Specifying the poistion of the SDRAM in ldscript where the binary file (kernel.img) will be stored.-------------------------"
 arm-none-eabi-objcopy main.elf -O binary `pwd`/boot/kernel.img
 echo "-------------------------Generating binary file from the executable file created with the description of the memory addresses of the peripherals connected.-------------------------"
 sudo ./rpiboot -d boot/
-echo "-------------------------Program uploaded. Enjoy!-------------------------" 
+echo "-------------------------Program uploaded. Enjoy!-------------------------"
